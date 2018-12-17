6787f953f203168c55249fee95b2440dbd96d288
KamBanwait <kam@scriptedpixels.co.uk>
2018-12-17 20:09:41 +0000

    placing spread inline for legibility

--- src/reducers/recipeReducer.js
+++ src/reducers/recipeReducer.js
@@ -8,16 +8,10 @@ const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case FETCH_RECIPES:
-      return {
-        ...state,
-        items: action.payload
-      }
+      return { ...state, items: action.payload }
 
     case NEW_RECIPE:
-      return {
-        ...state,
-        item: action.payload
-      }
+      return { ...state, item: action.payload }
 
     case REMOVE_RECIPE:
       const firstRecipe = state.indexOf(action.payload)
