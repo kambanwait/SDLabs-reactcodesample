0620be3d7635d25d662f1d3f331c14daa276dea1
KamBanwait <kam@scriptedpixels.co.uk>
2018-12-17 02:57:58 +0000

    moving reactredux provider & store to index.js as per best practices
    following tutorial stated that it’s better to have the store and provider on index.js and keep router on App.js to help keep code clean

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import { hot } from "react-hot-loader";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { Provider } from "react-redux";
-import store from './store';
-
 require('./mystyles.scss');
 
 import Navigation from './components/Navigation';
@@ -16,18 +13,16 @@ import Error from './pages/Error';
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div>
-            <Navigation />
-            <Switch>
-              <Route path="/" component={Home} exact />
-              <Route path="/admin" component={Admin} />
-              <Route component={Error} />
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/admin" component={Admin} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
