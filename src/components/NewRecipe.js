f88324f595b6adb7775d6a0ec68e54317c966d7f
KamBanwait <kam@scriptedpixels.co.uk>
2018-12-17 02:53:16 +0000

    Add connect to newRecipe mod so I can pass new recipe in to Redux

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createRecipe } from '../actions/recipeActions';
+
+
 class NewRecipe extends Component {
   state = {
     name: '',
@@ -8,57 +13,53 @@ class NewRecipe extends Component {
     steps: ''
   };
 
-  handleSubmit = event => {
-    event.preventDefault();
-
-    const recipe = {
-      title: this.state.title,
-      description: this.state.body,
-      ingredients: this.state.ingredients,
-      steps: this.state.steps
-    }
-  };
-
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.createRecipe(this.state);
+  };
+
   render() {
-    return (<section>
-      <h3 className="title is-3">Create your new recipe:</h3>
-      <form onSubmit={this.handleSubmit} >
-        <div className="field">
-          <label className="label">Recipe Name</label>
-          <div className="control">
-            <input className="input" type="text" placeholder="Recipe name" name="name" value={this.state.name} onChange={this.onChange} />
+    return (
+      <section>
+        <h3 className="title is-3">Create your new recipe:</h3>
+        <form onSubmit={this.handleSubmit} >
+          <div className="field">
+            <label className="label">Recipe Name</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="Recipe name" name="name" onChange={this.onChange} />
+            </div>
           </div>
-        </div>
 
-        <div className="field">
-          <label className="label">Description:</label>
-          <div className="control">
-            <input className="input" type="text" placeholder="Description" name="description" onChange={this.onChange} />
+          <div className="field">
+            <label className="label">Description:</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="Description" name="description" onChange={this.onChange} />
+            </div>
           </div>
-        </div>
 
-        <div className="field">
-          <label className="label">Ingredients:</label>
-          <div className="control">
-            <textarea className="textarea" placeholder="Ingredients" name="ingredients" onChange={this.onChange}></textarea>
+          <div className="field">
+            <label className="label">Ingredients:</label>
+            <div className="control">
+              <textarea className="textarea" placeholder="Ingredients" name="ingredients" onChange={this.onChange}></textarea>
+            </div>
           </div>
-        </div>
 
-        <div className="field">
-          <label className="label">Steps:</label>
-          <div className="control">
-            <textarea className="textarea" placeholder="Steps" name="steps" onChange={this.onChange}></textarea>
+          <div className="field">
+            <label className="label">Steps:</label>
+            <div className="control">
+              <textarea className="textarea" placeholder="Steps" name="steps" onChange={this.onChange}></textarea>
+            </div>
           </div>
-        </div>
 
-        <button type="submit" className="button" >Add new recipe</button>
-      </form>
-    </section>);
+          <button type="submit" className="button" >Add new recipe</button>
+        </form>
+      </section>
+    );
   }
 }
 
-export default NewRecipe;
+export default connect(null, { createRecipe })(NewRecipe);
