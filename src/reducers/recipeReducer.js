a79e477741a93e13c78835d8c1888d0168df190b
KamBanwait <kam@scriptedpixels.co.uk>
2018-12-17 23:18:36 +0000

    fix add_recipe to add new recipe in to existing array.
    Fixes naming from items to recipes - no longer need item anymore

--- src/reducers/recipeReducer.js
+++ src/reducers/recipeReducer.js
@@ -1,17 +1,20 @@
 import { FETCH_RECIPES, NEW_RECIPE, REMOVE_RECIPE } from '../actions/types';
 
 const initialState = {
-  items: [],
-  item: {}
+  recipes: []
 }
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case FETCH_RECIPES:
-      return { ...state, items: action.payload }
+      console.log(action.payload)
+      return { ...state, recipes: action.payload }
 
     case NEW_RECIPE:
-      return { ...state, item: action.payload }
+      return {
+        ...state,
+        recipes: [action.payload, ...state.recipes]
+      }
 
     case REMOVE_RECIPE:
       const firstRecipe = state.indexOf(action.payload)
