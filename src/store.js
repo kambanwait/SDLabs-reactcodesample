82a0ac8396f761864d4a9a08793b2dfdd3de3942
KamBanwait <kam@scriptedpixels.co.uk>
2018-12-17 03:00:10 +0000

    Store queries local storage on store change vis subscribe.
    Needs to be fixed to save new recipe in to local storage when it’s added as it’s stuck in Item on page reload

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react";
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
+import { saveState } from './localStorage';
 
-const initialState = {};
+const initialState = localStorage.getItem('recipes') ? JSON.parse(localStorage.getItem('recipes')) : {}
+
 const middleware = [thunk];
+
 const store = createStore(
   rootReducer,
   initialState,
@@ -13,5 +15,11 @@ const store = createStore(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ));
+
+store.subscribe(() => {
+  saveState({
+    recipes: store.getState().recipes
+  });
+})
 
 export default store;
