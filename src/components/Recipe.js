cfed507fc1dece04e506a1fcc9809571fa274185
KamBanwait <kam@scriptedpixels.co.uk>
2018-12-18 16:21:16 +0000

    small UI tweaks to each recipe shown in card

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -53,32 +53,30 @@ class Recipe extends Component {
       )
     }
     return (
-      <article className="column is-one-third">
-        <div className="card">
-          <header className="card-header">
-            <p className="card-header-title">{this.props.recipe.name}</p>
-          </header>
+      <div className="card">
+        <header className="card-header">
+          <p className="card-header-title">{this.props.recipe.name}</p>
+        </header>
 
-          <div className="card-image">
-            <figure className="image">
-              <img src={this.props.recipe.imageURL} alt="Placeholder image" />
-            </figure>
+        <div className="card-image">
+          <figure className="image">
+            <img src={this.props.recipe.imageURL} alt="Placeholder image" />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="content">
+            <p>{this.props.recipe.description}</p>
+            <br />
+            <h5 className="title is-5">Ingredients</h5>
+            <p>{this.props.recipe.ingredients}</p>
+            <br />
+            <h5 className="title is-5">Steps</h5>
+            <p>{this.props.recipe.steps}</p>
           </div>
-          <div className="card-content">
-            <div className="content">
-              <p>{this.props.recipe.description}</p>
-              <br />
-              <h5 className="title is-5">Ingredients</h5>
-              <p>{this.props.recipe.ingredients}</p>
-              <br />
-              <h5 className="title is-5">Steps</h5>
-              <p>{this.props.recipe.steps}</p>
-            </div>
-          </div>
-          {/* only show admin controls when we're on admin page */}
-          {this.props.showAdmin && <AdminControls index={this.props.index} toggleEdit={this.toggleEdit} recipe={this.props.recipe} removeRecipe={this.props.removeRecipe} editRecipe={this.props.editRecipe} />}
-        </div >
-      </article>
+        </div>
+        {/* only show admin controls when we're on admin page */}
+        {this.props.showAdmin && <AdminControls index={this.props.index} toggleEdit={this.toggleEdit} recipe={this.props.recipe} removeRecipe={this.props.removeRecipe} editRecipe={this.props.editRecipe} />}
+      </div >
     );
   }
 }
