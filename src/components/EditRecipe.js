9e2abb0e259605942bbe73dfa3caa15ad65e1be6
KamBanwait <kam@scriptedpixels.co.uk>
2018-12-18 11:29:46 +0000

    EditingRecipe form
    Use props to update parent component to fire update it’s state for the recipe it has been passed.
    
    OnSubmit is passed in via parent in props and we submit the new edits to the parent to then update redux

---
+++ src/components/EditRecipe.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+import PropTypes from 'prop-types';
+
+const EditRecipe = props => {
+  return (
+    <form onSubmit={props.handleSubmit} >
+      <div className="field">
+        <label className="label">Recipe Name</label>
+        <div className="control">
+          <input className="input" type="text" name="name" onChange={props.onChange} value={props.recipe.name} />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">Description:</label>
+        <div className="control">
+          <input className="input" type="text" placeholder="Description" name="description" onChange={props.onChange} value={props.recipe.description} />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">Ingredients:</label>
+        <div className="control">
+          <textarea className="textarea" placeholder="Ingredients" name="ingredients" onChange={props.onChange} value={props.recipe.ingredients} ></textarea>
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">Steps:</label>
+        <div className="control">
+          <textarea className="textarea" placeholder="Steps" name="steps" onChange={props.onChange} value={props.recipe.steps} ></textarea>
+        </div>
+      </div>
+
+      <button disabled={!props.recipe.steps} type="submit" className="button" >Update recipe</button>
+    </form>
+  );
+}
+
+export default EditRecipe;
