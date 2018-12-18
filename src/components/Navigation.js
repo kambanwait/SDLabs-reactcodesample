1ee09632b92c7c102dfc0ac74a5b1450a7ad7910
KamBanwait <kam@scriptedpixels.co.uk>
2018-12-18 14:11:15 +0000

    Fixes navigation/header styling and adds logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,13 +3,15 @@ import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
+    <nav className="navbar container is-fluid level is-primary" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <h1 className="title is-1">Recipe box</h1>
+        <NavLink className="navbar-item header-logo" to="/">
+          <img src="images/cook.svg" width="112" height="28" />
+          <p className="title is-4">Recipe Box</p>
+        </NavLink>
 
         <NavLink className="navbar-item" to="/">Home</NavLink>
         <NavLink className="navbar-item" to="/admin">Admin</NavLink>
-
       </div>
     </nav>
   );
